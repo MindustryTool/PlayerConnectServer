244e62c959d25130ba09613cdf28c168f6afbe23
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-07-29 21:27:39 +0700

    add http server

---
+++ src/playerconnect/HttpServer.java
@@ -0,0 +1,104 @@
+package playerconnect;
+
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import org.eclipse.jetty.util.thread.ExecutorThreadPool;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import arc.Events;
+import arc.util.Log;
+import io.javalin.Javalin;
+import io.javalin.json.JavalinJackson;
+import io.javalin.plugin.bundled.RouteOverviewPlugin;
+import mindustry.game.Gamemode;
+import playerconnect.shared.Packets;
+import io.javalin.http.sse.SseClient;
+
+public class HttpServer {
+
+    private Javalin app;
+
+    private final Queue<SseClient> statsConsumers = new ConcurrentLinkedQueue<>();
+
+    public HttpServer() {
+        app = Javalin.create(config -> {
+            config.showJavalinBanner = false;
+            config.jsonMapper(new JavalinJackson().updateMapper(mapper -> {
+                mapper//
+
+                        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)//
+                        .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)//
+                        .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+                        .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+            }));
+
+            config.http.asyncTimeout = 5_000;
+            config.useVirtualThreads = true;
+
+            ExecutorThreadPool pool = new ExecutorThreadPool(50, 0);
+            pool.setName("HttpServer");
+            config.jetty.threadPool = pool;
+            config.jetty.modifyServer(server -> server.setStopTimeout(5_000)); // wait 5 seconds for existing requests
+
+            config.registerPlugin(new RouteOverviewPlugin());
+
+            config.requestLogger.http((ctx, ms) -> {
+                if (!ctx.fullUrl().contains("stats")) {
+                    Log.info("[" + ctx.method().name() + "] " + Math.round(ms) + "ms " + ctx.fullUrl());
+                }
+            });
+
+            app.sse("workflow/events", client -> {
+                client.keepAlive();
+                client.sendComment("connected");
+
+                client.onClose(() -> {
+                    statsConsumers.remove(client);
+                });
+
+                statsConsumers.add(client);
+            });
+
+        });
+
+        app.start(Integer.parseInt(System.getenv("HTTP_PORT")));
+
+        Events.on(Packets.StatsPacket.class, event -> {
+            for (SseClient client : statsConsumers) {
+                StatsLiveEventData response = new StatsLiveEventData();
+                response.mapName = event.data.mapName;
+                response.name = event.data.name;
+                response.gamemode = event.data.gamemode;
+                response.mods = event.data.mods.list();
+                response.roomId = event.data.roomId;
+
+                for (Packets.StatsPacketPlayerData playerData : event.data.players) {
+                    StatsLiveEventPlayerData player = new StatsLiveEventPlayerData();
+                    player.name = playerData.name;
+                    player.locale = playerData.locale;
+                    response.players.add(player);
+                }
+                client.sendEvent(response);
+            }
+        });
+    }
+
+    public static class StatsLiveEventData {
+        public ArrayList<StatsLiveEventPlayerData> players = new ArrayList<>();
+        public String mapName = "unknown";
+        public String name = "";
+        public String gamemode = Gamemode.survival.name();
+        public ArrayList<String> mods = new ArrayList<>();
+        public String roomId = null;
+    }
+
+    public static class StatsLiveEventPlayerData {
+        public String name = "";
+        public String locale = "en";
+    }
+}
