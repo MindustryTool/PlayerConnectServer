f0292d2314d98e414d330330ccff44ac3a93e137
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-08-27 14:48:50 +0700

    fix(NetworkRelay): skip error logs for non-error disconnections
    Prevent logging disconnection messages when the reason is not an error to reduce noise in logs

--- src/playerconnect/NetworkRelay.java
+++ src/playerconnect/NetworkRelay.java
@@ -97,7 +97,9 @@ public class NetworkRelay extends Server implements NetListener {
 
     @Override
     public void disconnected(Connection connection, DcReason reason) {
-        Log.info("Connection @ lost: @.", Utils.toString(connection), reason);
+        if (reason != DcReason.error) {
+            Log.info("Connection @ lost: @.", Utils.toString(connection), reason);
+        }
 
         if (connection.getLastProtocolError() != null) {
             Log.err(connection.getLastProtocolError());
