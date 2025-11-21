06775aae6e4a8cf34612d4eb736824d560d34a42
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-11-21 20:57:57 +0700

    fix: correct IP logging in connection closure
    Use correct connection parameter in Utils.getIP() call when logging closed connections to ensure accurate IP address is recorded.

--- src/playerconnect/ServerRoom.java
+++ src/playerconnect/ServerRoom.java
@@ -110,7 +110,7 @@ public class ServerRoom implements NetListener {
                 p.connectionId = connectionId;
                 p.reason = DcReason.error;
                 host.sendTCP(p);
-                Log.info("Closed connection: " + con + " ip: " + Utils.getIp(con));
+                Log.info("Closed connection: " + con + " ip: " + Utils.getIP(connection));
             }
 
         } else if (host.isConnected() && clients.containsKey(connection.getID())) {
